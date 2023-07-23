01d48e625169fd1af8231238ac77303926d17dc3
BillGun <privatebillgun@gmail.com>
2023-07-23 10:37:03 +1000

    trying to fix capitalization

--- src/components/logo.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Logo() {
-  return (
-    <div className="flex items-center justify-center mt-2">
-      <Link href="/" className="w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-xl font-bold">BG</Link>
-    </div>
-  )
-}
